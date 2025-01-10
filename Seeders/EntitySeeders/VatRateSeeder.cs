d26b172d757afeef354cdf735b81a134dafc73bb
Krystian Pasierb-Suchy <suchykaes@gmail.com>
2025-01-10 17:24:12 +0100

    create and configure vatrate seeder

---
+++ Seeders/EntitySeeders/VatRateSeeder.cs
@@ -0,0 +1,51 @@
+﻿using InvoiceAPI.Entities;
+using InvoiceAPI.Persistance;
+
+namespace InvoiceAPI.Seeders.EntitySeeders
+{
+    public class VatRateSeeder
+    {
+        private readonly InvoiceAPIDbContext _dbContext;
+        public VatRateSeeder(InvoiceAPIDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.VatRates.Any())
+                {
+                    var rates = GetVatRates();
+                    _dbContext.VatRates.AddRange(rates);
+
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
+        }
+        private IEnumerable<VatRate> GetVatRates()
+        {
+            var rates = new List<VatRate>()
+            {
+                new VatRate()
+                {
+                    Rate = 0.23m
+                },
+                new VatRate()
+                {
+                    Rate = 0.08m
+                },
+                new VatRate()
+                {
+                    Rate = 0.05m
+                },
+                new VatRate()
+                {
+                    Rate = 0m
+                }
+            };
+            return rates;
+        }
+    }
+}

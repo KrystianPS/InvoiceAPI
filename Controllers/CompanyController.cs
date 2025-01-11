abd75f2972fa84f60ee6ccd91ac5d3a97acfe85e
Krystian Pasierb-Suchy <suchykaes@gmail.com>
2025-01-11 07:57:50 +0100

    set company controller w/get

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,23 @@
+﻿using InvoiceAPI.Entities;
+using InvoiceAPI.Persistance;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceAPI.Controllers
+{
+    [Route("api/company")]
+    public class CompanyController : ControllerBase
+    {
+        private readonly InvoiceAPIDbContext _dbContext;
+        public CompanyController(InvoiceAPIDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public ActionResult<List<Company>> GetAll()
+        {
+            var companies = _dbContext.Companies.ToList();
+
+            return Ok(companies);
+        }
+
+    }
+}

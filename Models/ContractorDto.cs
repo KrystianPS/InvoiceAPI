ba13c125a52fec67ac32f861d9613060d586af7a
Krystian Pasierb-Suchy <suchykaes@gmail.com>
2025-01-11 20:07:40 +0100

    add fields in dto contractor and company

--- Models/ContractorDto.cs
+++ Models/ContractorDto.cs
@@ -7,6 +7,7 @@
         public required string Name { get; set; }
         public int? TIN { get; set; }
         public string? EmailAdress { get; set; }
+        public string? Phone { get; set; }
         public string? City { get; set; }
         public string? AddresLine1 { get; set; }
         public string? AddresLine2 { get; set; }

6ac9dbdf51d63da94ab302caca935afb115a7151
Krystian Pasierb-Suchy <suchykaes@gmail.com>
2025-01-12 07:22:48 +0100

    refactor companty entity

--- Entities/CompanyAddressDetails.cs
+++ Entities/CompanyAddressDetails.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public required string AddressLine1 { get; set; }
-        public string? AddressLine2 { get; set; }
+        public required string AddressLine2 { get; set; }
         public required string City { get; set; }
         public required string PostalCode { get; set; }
         public required string Country { get; set; }

03363831d8f28b78b1196d3611e8a2d5a915a74e
Krystian Pasierb-Suchy <suchykaes@gmail.com>
2025-01-13 12:43:40 +0100

    refactor productCategory

--- Entities/ProductCategory.cs
+++ Entities/ProductCategory.cs
@@ -3,7 +3,7 @@
     public class ProductCategory
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
+        public string? Name { get; set; }
         public List<Product> Products { get; set; } = new List<Product>();
     }
 }
